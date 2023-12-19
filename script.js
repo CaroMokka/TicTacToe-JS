bd1b5a6a6af9b27d80e73e6d555a0db226d8c6eb
carolina <ci.arayagonzalez@gmail.com>
2023-12-19 12:18:51 -0300

    :sparkles: Add validate choices players

--- script.js
+++ script.js
@@ -37,30 +37,89 @@ const tileSelected = (tile) => {
     if (gameOver) {
       return;
     }
+
     let coords = tile.id.split("-");
     console.log(coords);
     let r = parseInt(coords[0]);
     let c = parseInt(coords[1]);
-    console.log(r, c);
-    board[r][c];
+
+    if (board[r][c] !== " ") {
+      return;
+    }
+
+    board[r][c] = currentPlayer;
     this.innerText = currentPlayer;
+
     if (currentPlayer === playerOne) {
       currentPlayer = playerTwo;
     } else currentPlayer = playerOne;
+    checkWinner();
   });
 };
 
-// el chico del video Kennny Yip
-// function tileSelected() {
-//   if (gameOver) {
-//     return;
-//   }
+const checkWinner = () => {
+  //horizonntal
+  for (let r = 0; r < 3; r++) {
+    if (
+      board[r][0] == board[r][1] &&
+      board[r][1] == board[r][2] &&
+      board[r][0] != " "
+    ) {
+      for (let i = 0; i < 3; i++) {
+        let tile = document.getElementById(r.toString() + "-" + i.toString());
+        console.log(tile);
+        tile.classList.add("winner");
+      }
+      gameOver = true;
+      return;
+    }
+  }
+  //vertical
+  for (let c = 0; c < 3; c++) {
+    if (
+      board[0][c] == board[1][c] &&
+      board[1][c] == board[2][c] &&
+      board[0][c] != " "
+    ) {
+      for (let i = 0; i < 3; i++) {
+        let tile = document.getElementById(i.toString() + "-" + c.toString());
+        tile.classList.add("winner");
+      }
+      gameOver = true;
+      return;
+    }
+  }
+  //diagonal right
+  if (
+    board[0][0] == board[1][1] &&
+    board[1][1] == board[2][2] &&
+    board[0][0] != " "
+  ) {
+    for (let i = 0; i < 3; i++) {
+      let tile = document.getElementById(i.toString() + "-" + i.toString());
+      tile.classList.add("winner");
+    }
+    gameOver = true;
+    return;
+  }
+  //diagonal left
+  if (
+    board[0][2] == board[1][1] &&
+    board[1][1] == board[2][0] &&
+    board[0][2] != " "
+  ) {
+    //0-2
+    let tile = document.getElementById("0-2");
+    tile.classList.add("winner");
+    //1-1
+    tile = document.getElementById("1-1");
+    tile.classList.add("winner");
+    //2-0
+    tile = document.getElementById("2-0");
+    tile.classList.add("winner");
+    gameOver = true;
+    return;
+  }
+};
 
-//   let coords = this.id.split("-");
-//   console.log(coords)
-//   let r = parseInt(coords[0]);
-//   let c = parseInt(coords[1]);
 
-//   board[r][c] = currentPlayer;
-//   this.innerText = currentPlayer;
-// };
